caa858e3f72b346c323b3e36c088101e177fb631
tchiteu <matheussantos.contato362@gmail.com>
2021-06-23 20:08:13 -0300

    refactor(pages/index): Corrigindo erros apontados pelo Eslint #PES-55

--- pages/index.vue
+++ pages/index.vue
@@ -53,16 +53,15 @@
 
 <style>
 	.container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
+		margin: 0 auto;
+		min-height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
 	}
 
 	.title {
-		font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
 		display: block;
 		font-weight: 300;
 		font-size: 100px;

3932a5b4e0d4582ebb9b0b8c9fae6b90d364bfb3
Nicolas Engler <nicolasengler@hotmail.com>
2021-09-06 17:15:00 -0300

    fix(dashboard): removing unnecessary code #PES-237

--- components/dashboards/AdminDashboard.vue
+++ components/dashboards/AdminDashboard.vue
@@ -1,8 +1,6 @@
 <template>
 	<section>
 		<OActionCard icon="account-plus" :description="Dictionary.users.getLabel('create_new_user')" :action="openNewUserModal" />
-
-		<OActionCard icon="account-plus" :description="Dictionary.users.getLabel('create_new_user')" :action="openUserDetailsModal" />
 	</section>
 </template>
 
@@ -18,10 +16,6 @@
 		methods: {
 			openNewUserModal() {
 				this.openModal({ modal: 'users/new' });
-			},
-
-			openUserDetailsModal() {
-				this.openModal({ modal: 'users/details', id: 'bc94a640-206b-4d8a-9b3c-540078320699' });
 			}
 		}
 	});

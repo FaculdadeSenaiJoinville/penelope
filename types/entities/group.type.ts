be3b4e98779fbf4eb31972e274f7ebb758e61c18
Nicolas Engler <nicolasengler@hotmail.com>
2021-11-07 14:40:07 -0300

    feature(groups): group details #PES-334

---
+++ types/entities/group.type.ts
@@ -0,0 +1,33 @@
+import { User } from '.';
+
+export type Group = {
+	id: string;
+
+	name: string;
+
+	description: string;
+
+	members: User[];
+
+	created_at: Date;
+
+	updated_at: Date;
+
+	created_by?: string;
+
+	updated_by?: string;
+};
+
+export class GroupDetails {
+	public name: string;
+
+	public description: string;
+
+	public members: User[];
+
+	constructor(user?: Group) {
+		this.name = user ? user.name : '';
+		this.description = user ? user.description : '';
+		this.members = user ? user.members : [];
+	}
+}
